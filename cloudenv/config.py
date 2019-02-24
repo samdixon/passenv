48805d3ff84081325caa904cde4674611a213672
Sam Dixon <samljdixon@gmail.com>
2019-02-23 23:48:46 -0500

    rename

--- cloudenv/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    def __init__(self, password_manager):
-        self.password_manager = password_manager
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="passenv",
+    version="0.0.1",
+    description="Store Api Keys and Environment Variables in your Password Manager",
+    license="MIT",
+    author="Sam Dixon",
+    author_email = "samueljacobdixon@icloud.com",
+    url="https://github.com/samdixon/passenv",
+
+)
